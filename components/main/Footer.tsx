fdc66803777cad5a05d56f09ef22a1b8c88ff852
Alex <alexrosainfo@gmail.com>
2025-02-05 21:37:47 +0100

    Porfolio Commit 1

---
+++ components/main/Footer.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import { SiWakatime } from "react-icons/si";
+import {
+  RxGithubLogo,
+  RxInstagramLogo,
+  RxLinkedinLogo,
+  RxEnvelopeClosed,
+  RxFileText,
+} from "react-icons/rx";
+
+const Footer = () => {
+  return (
+    <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
+      <div className="w-full flex flex-col items-center justify-center m-auto">
+        <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
+          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+            <div className="font-bold text-[24px]">Comunidad</div>
+
+            <a
+              href="https://github.com/AlexRosa16"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex flex-row items-center my-[15px] cursor-pointer"
+            >
+              <RxGithubLogo />
+              <span className="text-[15px] ml-[6px]">Github</span>
+            </a>
+
+            <a
+              href="https://wakatime.com/@AlexRosa16"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex flex-row items-center my-[15px] cursor-pointer"
+            >
+              <SiWakatime />
+              <span className="text-[15px] ml-[6px]">Wakatime</span>
+            </a>
+          </div>
+
+          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+            <div className="font-bold text-[24px]">Redes Sociales</div>
+
+            <a
+              href="https://www.instagram.com/alex_rosa16/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex flex-row items-center my-[15px] cursor-pointer"
+            >
+              <RxInstagramLogo />
+              <span className="text-[15px] ml-[6px]">Instagram</span>
+            </a>
+
+            <a
+              href="https://linkedin.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex flex-row items-center my-[15px] cursor-pointer"
+            >
+              <RxLinkedinLogo />
+              <span className="text-[15px] ml-[6px]">Linkedin</span>
+            </a>
+          </div>
+
+          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+            <div className="font-bold text-[24px]">Sobre mí</div>
+
+            <a
+              href="mailto:alexrosainfo@gmail.com"
+              className="flex flex-row items-center my-[15px] cursor-pointer"
+            >
+              <RxEnvelopeClosed />
+              <span className="text-[15px] ml-[6px]">alexrosainfo@gmail.com</span>
+            </a>
+
+            <a
+              href="/CV_RosaGarcia_Alex.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex flex-row items-center my-[15px] cursor-pointer"
+            >
+              <RxFileText />
+              <span className="text-[15px] ml-[6px]">Curriculum</span>
+            </a>
+          </div>
+        </div>
+
+        <div className="mb-[20px] text-[15px] text-center">
+          &copy; Alex Rosa Garcia 2025 Inc.
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ constants/index.ts
@@ -0,0 +1,157 @@
+export const Skill_data = [
+  {
+    skill_name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Java",
+    Image: "/java.svg",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Python",
+    Image: "/python.svg",
+    width: 120,
+    height: 60,
+  },
+  {
+    skill_name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Framer Motion",
+    Image: "/framer.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Node js",
+    Image: "/node-js.png",
+    width: 80,
+    height: 80,
+  },
+
+];
+
+export const Socials = [
+  {
+    name: "Github",
+    src: "/github.svg",
+    link: "https://github.com/AlexRosa16"
+  },
+  {
+    name: "Linkedin",
+    src: "/linkedin.svg",
+    link: "https://www.linkedin.com/in/alex-rosa-garcia-aa5328310/"
+  },
+  {
+    name: "Wakatime",
+    src: "/wakatime.svg",
+    link: "https://wakatime.com/@AlexRosa16"
+  },
+];
+
+
+
+export const Frontend_skill = [
+ 
+  {
+    skill_name: "Material UI",
+    Image: "/mui.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Java Script",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Type Script",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next js 13",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+];
+
+export const Backend_skill = [
+  {
+    skill_name: "Laravel",
+    Image: "/laravel.svg",
+    width: 55,
+    height: 55,
+  },
+  
+  {
+    skill_name: "Fire base",
+    Image: "/Firebase.png",
+    width: 55,
+    height: 55,
+  },
+  {
+    skill_name: "PHP",
+    Image: "/php.svg",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "My SQL",
+    Image: "/mysql.png",
+    width: 70,
+    height: 70,
+  },
+];
+
+export const Full_stack = [
+  {
+    skill_name: "React Native",
+    Image: "/ReactNative .png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Docker",
+    Image: "/docker.webp",
+    width: 70,
+    height: 70,
+  },
+
+  {
+    skill_name: "Figma",
+    Image: "/figma.png",
+    width: 50,
+    height: 50,
+  },
+
+];
+
+
+
+
+
+
+
